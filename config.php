5dccf683054a6877033af6234d4af7a7f63c3276
Gabe Scott <gabescott@gmail.com>
2022-09-25 23:59:53 -0700

    Add much more thorough documentation and comments

--- config.php
+++ config.php
@@ -5,11 +5,19 @@ require_once '../vendor/autoload.php';  // relative path may no longer be correc
 
 // load GCS library
 use Google\Cloud\Storage\StorageClient;
+// load PHPSpreadsheet xlsx and csv tools
 use \PhpOffice\PhpSpreadsheet\Reader\Xlsx;
 use \PhpOffice\PhpSpreadsheet\Writer\Csv;
 
+// Path to private API key stored on GCP VM (not publicly)
+// TODO: encrypt file?
 $privateKeyFilePath = '../keys/silken-reducer-359320-b3cecc9b17ca.json';
 
+/**
+ *  Upload file to Google Cloud Storage Bucket
+ * 
+ *  @return bool - true if file is uploaded, false if not
+ */
 function uploadFile($bucketName, $fileContent, $cloudPath) 
 {
     $privateKeyFilePath = $GLOBALS['privateKeyFilePath'];
@@ -41,6 +49,9 @@ function uploadFile($bucketName, $fileContent, $cloudPath)
     return $storageObject != null;  // return if successful or not
 }
 
+/**
+ *  Get File info for specific file in Google Cloud Storage Bucket
+ */
 function getFileInfo($bucketName, $cloudPath) {
     $privateKeyFilePath = $GLOBALS['privateKeyFilePath'];
     // connect to Google Cloud Storage using private key as authentication
@@ -59,8 +70,9 @@ function getFileInfo($bucketName, $cloudPath) {
     return $object->info();
 }
 
-//list files in Google storage bucket
-/*
+/**
+ *  List all items in Google Cloud storage bucket
+ */
 function listFiles($bucket, $directory = null) {
 
     if ($directory == null) {
@@ -76,8 +88,14 @@ function listFiles($bucket, $directory = null) {
         // NOTE: if $object->name() ends with '/' then it is a 'folder'
     }
 }
-*/
-            /*
+/**
+ *  From Google Cloud Storage API Documentation
+ * 
+ * @param $bucketName - name of bucket (xlsx-uploads)
+ * @param $cloudPath - path of file on GCS
+ * 
+ * @return bool or storage object containing file info
+ */
 function downloadFileToMemory($bucketName, $cloudPath) {
     $privateKeyFileContent = $GLOBALS['privateKeyFileContent'];
     // connect to Google Cloud Storage using private key as authentication
@@ -102,9 +120,13 @@ function downloadFileToMemory($bucketName, $cloudPath) {
     );
     return $contents;
 }
-        */
 
-// Download file from GCS storage to local disk (on VM)
+/**
+ *  Download file from GCS storage to local disk (on VM)
+ * 
+ *  @return true if object is downloaded correctly
+ *  @return false if StorageClient fails to be created (Google Cloud Storage API call)
+ */
 function downloadLocally($bucketName, $cloudPath, $localpath)
 {
     //$bucketName = 'xlsx-uploads'; $temp = downloadLocally($bucketName, $cloudPath, $localpath);
@@ -119,7 +141,7 @@ function downloadLocally($bucketName, $cloudPath, $localpath)
         ]);
     } catch (Exception $e) {
         // maybe invalid private key ?
-        print $e;
+        echo $e;
         return false;
     }
     $bucket = $storage->bucket($bucketName);
@@ -128,7 +150,13 @@ function downloadLocally($bucketName, $cloudPath, $localpath)
     return true; // return object (Psr\Http\Message\StreamInterface)
 }
 
-// use mysqli extension to connect to MySQL DB
+/**
+ * Use mysqli extension to connect to MySQL database
+ * 
+ * @return null if json_decode() fails
+ * @return null if mysqli object isn't created (connection to MySQL DB failed)
+ * @return mysqli object if MySQL database connection was successful
+ */
 function connectToDB()
 {
     //mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
